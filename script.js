e67a1f274e7efaaf7b44bcec312eb35f60ff7dd2
Rafiq <jalil716@gmail.com>
2025-04-01 18:59:53 -0700

    The querySelector lets you pick one element from your html

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let titleElement = document.getElementById("title");
 console.log(titleElement);
+
+let title2 = document.querySelector("title2")
+let title1 = document.querySelector("p")
+let paragraph = document.querySelector("paragraph")
